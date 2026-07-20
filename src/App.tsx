5ef9c315824bf5ba3e682680f24cbb16acfa16f9
Rafael Mendonça Duarte <duarterafael533@gmail.com>
2026-07-20 14:05:52 -0300

    Add libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 export default function App() {
   return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Meu Portfólio</h1>
-      <p>Em construção para a v1...</p>
+    <main className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-400">
+        Em desenvolvimento....
+      </h1>
     </main>
   );
 }
